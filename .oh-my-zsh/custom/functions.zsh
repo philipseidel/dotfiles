4af0b5c406a4f6f09d82ed849cdf340dbfccdad1
Philip Seidel <philipseidel@yahoo.com>
2013-04-09 14:02:47 -0400

    some functions

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -36,3 +36,47 @@ function perc() {
     result=`echo "scale=2;(($2 - $1) / $1)*100" | bc`
     echo "$result%"
 }
+
+# return a range of properly padded numbers
+function range() {
+    if [ $2 -lt 10 ]
+    then
+        flags='-f %02G'
+    else
+        flags='-w'
+    fi
+
+    if [ `uname` = "Darwin" ]
+    then
+        # os x uses gnu coreutils
+        gseq $flags $1 $2
+    else
+        seq $flags $1 $2
+    fi
+}
+
+# get a list of ip addresses with host names
+function lookup() {
+    for i in `range $2 $3`
+    do
+        host="$1$i$4"
+        ip=`dig +nocmd +nostats +nocomments +nomultiline $host | grep -Eo "([0-9]+\.[0-9]+\.)[0-9]+\.[0-9]+"`
+        echo "$host,$ip"
+    done;
+}
+
+# get a list of ip addresses 
+function iplist() {
+    for i in `lookup $1 $2 $3 $4`
+    do    
+        result=`echo "$i" | cut -d "," -f 2`
+        if [ "$output" = "" ]
+        then
+            output="$result"
+        else
+            output="$output $result"
+        fi
+    done
+    echo $output
+}
+
